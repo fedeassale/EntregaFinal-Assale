210086b9de1c784fd1e347349d976598a6ff08b8
fedeassale <fede_assale17@hotmail.com>
2023-10-22 13:43:40 -0300

    entregafinal1

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
+// async function obtenerproductos(){
+//     const response = await fetch ('./productos.json');
+//     if(response.ok){
+//         listadeproductos = await response.json();
+//         console.log(listadeproductos);
+//     }
+// }
+// cargarproductos();
+// cargarproductos = async ()=>{
+//     await obtenerproductos();
+// }
+
+
+
+
 
 const carrito=[];
 
@@ -5,14 +20,15 @@ let boton= document.getElementById("btn-carrito")
 boton.addEventListener("click",respuestaClick)
 
 function respuestaClick(){
-let opcion=1
+// let opcion=1
 // esto de acumular cantidad lo pude hacer funcionar solo en un producto, cuando lo pongo en varios me tira error, en la correccion me pueden indicar como se podria hacer?? gracias
 let indicecarrito = carrito.findIndex((itemcarrito)=> itemcarrito.nombre)
 
 if(indicecarrito !== -1){
     carrito[indicecarrito].cantidad++;
 }else{
-    carrito.push(new itemcarrito(listadeproductos [opcion-1].nombre,listadeproductos[opcion-1].marca,listadeproductos[opcion-1].precio,listadeproductos[opcion-1].cantidad));
+    // carrito.push(new itemcarrito(listadeproductos [opcion-1].nombre,listadeproductos[opcion-1].marca,listadeproductos[opcion-1].precio,listadeproductos[opcion-1].cantidad));
+    carrito.push(new itemcarrito(listadeproductos [0].nombre,listadeproductos[0].marca,listadeproductos[0].precio,listadeproductos[0].cantidad));
 }
 
 
@@ -26,9 +42,8 @@ let boton2= document.getElementById("btn-carrito2")
 boton2.addEventListener("click",respuestaClick2)
 
 function respuestaClick2(){
-let opcion2=2
-
-carrito.push(new itemcarrito(listadeproductos [opcion2-1].nombre,listadeproductos[opcion2-1].marca,listadeproductos[opcion2-1].precio,listadeproductos[opcion2-1].cantidad));
+    // let opcion2=2
+carrito.push(new itemcarrito(listadeproductos [1].nombre,listadeproductos[1].marca,listadeproductos[1].precio,listadeproductos[1].cantidad));
 
 tabla()
 
@@ -53,14 +68,15 @@ function tabla(){
     const total = document. querySelector(`#total`);
     bodytabla.innerHTML=``;
     carrito.forEach((itemcarrito,index)=>{
+        const{nombre,marca,cantidad,precio}=itemcarrito
         bodytabla.innerHTML =bodytabla.innerHTML+`
         <tr>
             <th scope="row">${index+1}</th>
-            <td>${itemcarrito.nombre}</td>
-            <td>${itemcarrito.marca}</td>
-            <td>${itemcarrito.cantidad}</td>
-            <td>${itemcarrito.precio}</td>
-            <td>${itemcarrito.cantidad*itemcarrito.precio}</td>
+            <td>${nombre}</td>
+            <td>${marca}</td>
+            <td>${cantidad}</td>
+            <td>${precio}</td>
+            <td>${cantidad*precio}</td>
         </tr>
         `
         total.textContent = carrito.reduce((acc,item)=> acc+item.cantidad*item.precio,0);
